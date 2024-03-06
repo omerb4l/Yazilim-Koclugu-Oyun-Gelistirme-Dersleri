c785ac6a2044b885f01a695044cb4b8c91d5fb15
Ömer BAL <omeerb4l@gmail.com>
2024-03-06 16:25:57 +0300

    Hafta-3 Dersi
    Yazılım Koçluğu Programı dahilinde bu hafta soru-cevap uygulamasının temellerini oluşturduk.

--- Oyun Geliştirme Projelerim/FutbolSahasi/Assets/Ders-3/UcuncuDersKod.cs
+++ Oyun Geliştirme Projelerim/FutbolSahasi/Assets/Ders-3/UcuncuDersKod.cs
@@ -1,18 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UcuncuDersKod : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+
+    public Text soruBasligiMetni;
+    public Text sorununMetni;
+    public int soruNumarasi;
+    public Text soruNumarasiMetni;
+
+    public Text aSikkiMetni;
+    public Text bSikkiMetni;
+
+    public string dogruCevap;
+
+    public Image aSikkiDogrulukResmi;
+    public Image bSikkiDogrulukResmi;
+
+    private void Start()
     {
-        
+        SoruUret();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+        if (true)
+        {
+
+        }
+
+
+
+
     }
+
+    public void DogrulukKontrolu(string cevap)
+    {
+        dogruCevap = "Ýlham";
+        if (dogruCevap == cevap)
+        {
+            sorununMetni.text = "Tebrikler! Soruyu doðru cevaplandýrdýrdýnýz.";
+            aSikkiDogrulukResmi.color = Color.green;
+            bSikkiDogrulukResmi.color = Color.white;
+
+
+        }
+        else
+        {
+            sorununMetni.text = "Maalesef! Soruyu yanlýþ cevaplandýrdýnýz.";
+            aSikkiDogrulukResmi.color = Color.white;
+            bSikkiDogrulukResmi.color = Color.red;
+        }
+
+    }
+    public void SoruUret() {
+
+        aSikkiDogrulukResmi.color = Color.white;
+        bSikkiDogrulukResmi.color = Color.white;
+
+        soruBasligiMetni.text = "Genel Kültür/Teknoloji";
+        sorununMetni.text = "Inspimonun kelime anlamý aþaðýdakilerden hangisidir?";
+        soruNumarasi = 1;
+        soruNumarasiMetni.text = soruNumarasi.ToString();
+
+        aSikkiMetni.text = "A)" + "Ýlham";
+        bSikkiMetni.text = "B)" + "Ýntikam";
+
+    }
+
 }
